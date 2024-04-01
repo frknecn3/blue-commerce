0db5377741fad8c6f1aaeb62f57e1b7b562b2e8d
Furkan Ercan <furkanduhannuman@gmail.com>
2024-04-01 04:04:09 +0300

    added featured product & minor improvements for the fourth time

--- src/components/FeaturedProduct/FeaturedProductButton.tsx
+++ src/components/FeaturedProduct/FeaturedProductButton.tsx
@@ -1,15 +1,19 @@
 'use client'
 
+import { useDispatch } from 'react-redux'
 import { ProductParams, User } from '../../constants/constants'
 import { getActiveUserFromStorage } from '../../utils/clientOnlyUtils'
-import { addToCart } from '../../utils/utils'
+import { addToAnonymousCart, addToCart, reloadCart } from '../../utils/utils'
+import { useEffect, useState } from 'react'
 
 const FeaturedProductButton = ({featuredProduct}:{featuredProduct:ProductParams}) => {
+  const dispatch = useDispatch()
 
-  const user = getActiveUserFromStorage().uid
+    const user = getActiveUserFromStorage()
+
   
   return (
-    <button onClick={()=>{addToCart(user,featuredProduct.id)}} className="p-4 rounded-xl w-full relative text-white hover:translate-y-[-2px] hover:brightness-125 transition-all bg-green-500">
+    <button onClick={()=>{try{user?addToCart(user.uid,featuredProduct.id):addToAnonymousCart(featuredProduct.id),reloadCart(user.uid,dispatch)}catch(err){console.log(err)}}} className="p-4 rounded-xl w-full relative text-white hover:translate-y-[-2px] hover:brightness-125 transition-all bg-green-500">
       <div className="flex justify-content-end items-end"><span className="line-through opacity-50 absolute left-4 top-4">${featuredProduct.price*1.2}</span></div> <div className="mt-4 ml-4"><span> ${featuredProduct.price}</span></div>
     </button>
   )
--- src/utils/clientOnlyUtils.ts
+++ src/utils/clientOnlyUtils.ts
@@ -1,15 +1,12 @@
-'use client'
-
 import { signOut, User } from "firebase/auth";
 import { auth } from "../firebase/config";
 
 export const getActiveUserFromStorage = ():User => {
     if (typeof window !== 'undefined') {
       // Check if the code is running in a browser environment
-      return JSON.parse(localStorage.getItem('user'));
+      const user:User = JSON.parse(localStorage.getItem('user'));
+      return  user;
     }
-  
-    return null; // Return an appropriate value if not in a browser environment
   };
 
   export const handleLogOut = async():Promise<void> =>{
